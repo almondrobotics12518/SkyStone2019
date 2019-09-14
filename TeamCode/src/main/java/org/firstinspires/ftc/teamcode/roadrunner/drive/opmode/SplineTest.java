1bd3600071d8294ba8861c34a0612bf9a7c69797
Hydralyncs <39605074+Hydralyncs@users.noreply.github.com>
2019-09-14 09:51:00 -0400

    Added roadrunner quickstart.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunner/drive/opmode/SplineTest.java
@@ -0,0 +1,39 @@
+package org.firstinspires.ftc.teamcode.roadrunner.drive.opmode;
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+
+import org.firstinspires.ftc.teamcode.roadrunner.drive.tank.SampleTankDriveBase;
+import org.firstinspires.ftc.teamcode.roadrunner.drive.tank.SampleTankDriveREVOptimized;
+
+/*
+ * This is an example of a more complex path to really test the tuning.
+ */
+@Autonomous(group = "drive")
+public class SplineTest extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        SampleTankDriveBase drive = new SampleTankDriveREVOptimized(hardwareMap);
+
+        waitForStart();
+
+        if (isStopRequested()) return;
+
+        drive.followTrajectorySync(
+                drive.trajectoryBuilder()
+                        .splineTo(new Pose2d(30, 30, 0))
+                        .build()
+        );
+
+        sleep(2000);
+
+        drive.followTrajectorySync(
+                drive.trajectoryBuilder()
+                        .reverse()
+                        .splineTo(new Pose2d(0, 0, 0))
+                        .build()
+        );
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunner/drive/opmode/TurnTest.java
@@ -0,0 +1,29 @@
+package org.firstinspires.ftc.teamcode.roadrunner.drive.opmode;
+
+import com.acmerobotics.dashboard.config.Config;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+
+import org.firstinspires.ftc.teamcode.roadrunner.drive.tank.SampleTankDriveBase;
+import org.firstinspires.ftc.teamcode.roadrunner.drive.tank.SampleTankDriveREVOptimized;
+
+/*
+ * This is a simple routine to test turning capabilities.
+ */
+@Config
+@Autonomous(group = "drive")
+public class TurnTest extends LinearOpMode {
+    public static double ANGLE = 90;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        SampleTankDriveBase drive = new SampleTankDriveREVOptimized(hardwareMap);
+
+        waitForStart();
+
+        if (isStopRequested()) return;
+
+        drive.turnSync(Math.toRadians(ANGLE));
+    }
+}
