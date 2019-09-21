645debd662cda61fbe38a0d3f10fbaec9c871887
Hydralyncs <39605074+Hydralyncs@users.noreply.github.com>
2019-09-21 08:05:55 -0400

    Changed back to old localizer.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auto/BlueDepotSide.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auto/BlueDepotSide.java
@@ -37,8 +37,6 @@ public class BlueDepotSide extends AlmondLinear {
 
         turnTo(Math.PI/2);
 
-        
-
         drive.followTrajectory(second);
 
         drive.followTrajectorySync(drive.trajectoryBuilder()
