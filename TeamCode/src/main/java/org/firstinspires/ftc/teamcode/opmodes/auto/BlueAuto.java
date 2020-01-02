3a9c02b2d94b8d06a588d67adebb9743f02da761
iheat <almondrobotics@gmail.com>
2020-01-02 17:42:05 -0500

    added auto files

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auto/BlueAuto.java
@@ -0,0 +1,16 @@
+package org.firstinspires.ftc.teamcode.opmodes.auto;
+
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+import org.firstinspires.ftc.teamcode.opmodes.AlmondLinear;
+
+@Autonomous(group="auto",name="Blue Auto")
+public class BlueAuto extends AlmondLinear {
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+    }
+
+}
