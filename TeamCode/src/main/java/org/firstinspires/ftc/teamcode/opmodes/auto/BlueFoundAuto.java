8f084e263fd64d0af9ff4d55c967efb5b9cd66a2
tencups <michaelwwang17@gmail.com>
2020-01-25 15:17:29 -0500

    added toggle functions and finished teleop

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auto/BlueFoundAuto.java
@@ -0,0 +1,78 @@
+package org.firstinspires.ftc.teamcode.opmodes.auto;
+
+import android.os.Build;
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
+import com.acmerobotics.roadrunner.path.heading.ConstantInterpolator;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.robotcore.external.Const;
+import org.firstinspires.ftc.teamcode.subsystems.DriveTrain;
+import org.firstinspires.ftc.teamcode.subsystems.Hook;
+
+import java.util.Vector;
+@Autonomous()
+public class BlueFoundAuto extends LinearOpMode {
+
+    private DriveTrain drive;
+    private Hook hook;
+
+    public void runOpMode() throws InterruptedException {
+
+        drive = new DriveTrain(hardwareMap);
+        hook = new Hook(hardwareMap);
+
+        drive.setPoseEstimate(new Pose2d(40, 62, Math.toRadians(90)));
+        waitForStart();
+
+        drive.followTrajectory(drive.trajectoryBuilder()
+                .back(1)
+                .reverse()
+                .splineTo(new Pose2d(50, 35, Math.toRadians(90)))
+                .lineTo(new Vector2d(50, 30), new ConstantInterpolator(Math.toRadians(90)))
+                .build());
+
+        while (!isStopRequested() && drive.isBusy()) {
+            drive.update();
+        }
+
+        hook.close();
+        sleep(500);
+
+        drive.followTrajectory(drive.trajectoryBuilder()
+                .lineTo(new Vector2d(40, 53), new ConstantInterpolator(Math.toRadians(90)))
+                .build());
+        while (!isStopRequested() && drive.isBusy()) {
+            drive.update();
+        }
+
+        drive.turn(Math.toRadians(90));
+        while (!isStopRequested() && drive.isBusy()) {
+            drive.update();
+        }
+
+        drive.followTrajectory(drive.trajectoryBuilder()
+                .lineTo(new Vector2d(80, 46), new ConstantInterpolator(Math.toRadians(180)))
+                .build());
+
+        while (!isStopRequested() && drive.isBusy()) {
+            drive.update();
+        }
+
+        hook.open();
+        sleep(300);
+
+
+        drive.followTrajectory(drive.trajectoryBuilder()
+                .lineTo(new Vector2d(drive.getPoseEstimate().getX() - 40, 34), new ConstantInterpolator(Math.toRadians(180)))
+                .build());
+        while (!isStopRequested() && drive.isBusy()) {
+        drive.update();
+        }
+
+
+
+    }
+}
